0636a0b4fddecfb369546a51726dd085868e2c5c
Jean Merlet <jean.joseph.merlet@gmail.com>
2016-04-07 13:31:45 -0400

    fixed feedback bugs

--- mastermind.rb
+++ mastermind.rb
@@ -4,17 +4,29 @@ class Mastermind
     @player = Player.new
   end
 
-  def play
+  def intro
     @board = Board.new
     @turn = 0
 
+    puts "Welcome to Mastermind!\n\n"
     @board.display
-    puts "\nIntro"
+    puts "\nYour goal is to guess the secret code consisting of 4 ordered colors within 12 turns."
+    puts "Each guess will receive one + each time both color and spot are guessed correctly, and one - per correctly guessed color in the wrong spot."
+    puts "\nType your guess as 4 capital letters in a row. The options are:"
+    puts "(B)lue, (G)reen, (O)range, (P)urple, (R)ed, and (W)hite\n\n"
+  end
+
+  def play
+    intro
 
     until @turn == 12 || @board.solved?
       guess = @player.guess
       @board.update_guess_history(guess, @turn)
+      @board.update_feedback_history(@turn)
       @board.display
+
+      puts "\nPlease type 4 of RGBOPW for your guess:"
+
       @turn += 1
     end
 
@@ -48,16 +60,39 @@ class Mastermind
       @colors = ["B", "G", "O", "P", "R", "W"]
       @code = Array.new(4) { @colors[rand(6)] }
       @guess_history = Array.new(12) { [" ", " ", " ", " "] }
+      @feedback_history = Array.new(12) { [" ", " ", " ", " "] }
     end
 
     def display
       12.times do |i|
-        print "\n ---------\n"
+        print "\n ------------------\n"
         print "| "
         4.times { |j| print "#{@guess_history[i][j]} " }
         print "|"
+        4.times { |j| print "#{@feedback_history[i][j]} " }
+        print "|"
       end
-      print "\n ---------\n"
+      print "\n ------------------\n"
+    end
+
+    def update_feedback_history(turn)
+      @feedback_history[turn] = []
+      code = @code
+
+      4.times do |i|
+        if @guess_history[turn][i] == @code[i]
+          @feedback_history[turn] << "+" 
+          code[i] = 0
+        end
+      end
+
+      code.delete(0)
+
+      code.length.times do |i|
+        @feedback_history[turn] << "-" if code.include?(@guess_history[turn][i])
+      end
+
+      (4 - @feedback_history[turn]).length.times { @feedback_history[turn] << " " }
     end
 
     def update_guess_history(guess, turn)
@@ -65,8 +100,7 @@ class Mastermind
     end
 
     def solved?
-      puts "in solved"
-      @guess_history.last == @code
+      @guess_history.include?(@code)
     end
 
   end
@@ -74,13 +108,18 @@ class Mastermind
   class Player
 
     def guess
-      puts "in guess"
       loop do
         input = gets.chomp
         case input
           when /^[B, G, O, P, R, W]{4}$/ then return input
         end
       end
+    end
+
+    class AI < Player
+    end
+
+    class Human < Player
     end
       
   end
