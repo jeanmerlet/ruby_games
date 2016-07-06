004f917deb2f76067527d1ed40bbdab5d654e66e
Jean Merlet <jean.joseph.merlet@gmail.com>
2016-07-06 09:48:23 -0400

    initial knight commit

---
+++ chess/knight_movement.rb
@@ -0,0 +1,21 @@
+class Board
+
+  def initialize
+    
+  end
+
+  class Knight
+
+    def initialize
+    end
+
+    def knight_moves(start, destination)
+      
+    end
+
+  end
+
+end
+
+board = Board.new
+knight_moves([1, 1], [8, 8])
