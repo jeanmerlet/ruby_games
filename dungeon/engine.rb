c794625b4126b2569ed833563f87576ef0ec676f
Jean Merlet <jean.joseph.merlet@gmail.com>
2019-06-13 18:00:57 -0400

    print '@' in center of screen

--- dungeon/engine.rb
+++ dungeon/engine.rb
@@ -1,11 +1,19 @@
 require './lib/BearLibTerminal/BearLibTerminal.rb'
 
-Terminal.open
+@screen_height, @screen_width = '80', '50'
+@player_x, @player_y = @screen_width.to_i/2, @screen_height.to_i/2
 
-while Terminal.read == Terminal::TK_CLOSE
+Terminal.open
+Terminal.set("window: title='meow', size=#{@screen_height}x#{@screen_width}")
+#Terminal.set("font: ./tiles/arial10x10.png, size=10x10, format=truetype")
+@close = false
+
+while !@close
   Terminal.clear
-  Terminal.print(1, 1, '@')
+  Terminal.print(@player_y, @player_x, "@")
   Terminal.refresh
+  @input = Terminal.read
+  @close = true if @input == Terminal::TK_CLOSE
 end
 
 Terminal.close
