775f850482a73935459c62eabb20fb0bf9c0938e
Jean Merlet <jean.joseph.merlet@gmail.com>
2019-06-19 10:35:05 -0400

    add tunnel generator

--- dungeon/map_objects/game_map.rb
+++ dungeon/map_objects/game_map.rb
@@ -5,11 +5,19 @@ class GameMap
     @width = width
     @height = height
     @tiles = initialize_tiles
-    create_room(Rect.new(1, 1, 5, 5))
+    generate_map
   end
 
   def initialize_tiles
     Array.new(@width) { Array.new(@height) { Tile.new(true) } }
+  end
+
+  def generate_map
+    room1 = Rect.new(20, 15, 10, 15)
+    room2 = Rect.new(35, 15, 10, 15)
+    create_room(room1)
+    create_room(room2)
+    create_h_tunnel(25, 40, 23)
   end
 
   def create_room(room)
@@ -18,6 +26,16 @@ class GameMap
         tile.blocked = false
       end
     end
+  end
+
+  def create_h_tunnel(x1, x2, y)
+    @tiles[x1..x2+1].each do |h_tile_line|
+      h_tile_line[y].blocked = false
+    end
+  end
+
+  def create_v_tunnel(y1, y2, x)
+    @tiles[x][y1..y2+1].each { |tile| tile.blocked = false }
   end
 
   def blocked?(x, y)
