c89290ef0e2b2fdea1ce5e453002552a5f87ee38
Jean Merlet <jean.joseph.merlet@gmail.com>
2019-06-20 01:34:33 -0400

    map room and tunnel procedural generation added

--- dungeon/map_objects/game_map.rb
+++ dungeon/map_objects/game_map.rb
@@ -5,37 +5,68 @@ class GameMap
     @width = width
     @height = height
     @tiles = initialize_tiles
-    generate_map
   end
 
   def initialize_tiles
     Array.new(@width) { Array.new(@height) { Tile.new(true) } }
   end
 
-  def generate_map
-    room1 = Rect.new(20, 15, 10, 15)
-    room2 = Rect.new(35, 15, 10, 15)
-    create_room(room1)
-    create_room(room2)
-    create_h_tunnel(25, 40, 23)
+  def generate_level(min_length, max_length, max_rooms, player)
+    num_rooms = 0
+    rooms = []
+    while num_rooms < max_rooms
+      w = min_length + rand(max_length - min_length + 1)
+      h = min_length + rand(max_length - min_length + 1)
+      x, y = rand(@width - w), rand(@height - h)
+      new_room = Rect.new(x, y, w, h)
+
+      if !any_intersection?(rooms, new_room)
+        create_room(new_room) 
+        new_x, new_y = *new_room.center
+        if num_rooms == 0
+          player.x, player.y = new_x + 1, new_y + 1
+        else
+          prev_x, prev_y = *rooms.last.center
+          toss = rand(1)
+          if true #toss == 0
+            create_h_tunnel(prev_x, new_x, prev_y)
+            create_v_tunnel(prev_y, new_y, new_x)
+          else
+            #create_v_tunnel(prev_y, new_y, prev_x)
+            #create_h_tunnel(prev_x, new_x, new_y)
+          end
+        end
+        rooms << new_room
+        num_rooms += 1
+      end
+    end
+  end
+
+  def any_intersection?(rooms, new_room)
+    rooms.each do |room|
+      return true if new_room.intersect?(room)
+    end
+    false
   end
 
   def create_room(room)
-    @tiles[room.x1+1..room.x2-1].each do |h_tile_line|
-      h_tile_line[room.y1+1..room.y2-1].each do |tile|
+    @tiles[room.x1+1..room.x2].each do |h_tile_line|
+      h_tile_line[room.y1+1..room.y2].each do |tile|
         tile.blocked = false
       end
     end
   end
 
   def create_h_tunnel(x1, x2, y)
-    @tiles[x1..x2+1].each do |h_tile_line|
+    x1, x2 = x2, x1 if x1 > x2
+    @tiles[x1..x2].each do |h_tile_line|
       h_tile_line[y].blocked = false
     end
   end
 
   def create_v_tunnel(y1, y2, x)
-    @tiles[x][y1..y2+1].each { |tile| tile.blocked = false }
+    y1, y2 = y2, y1 if y1 > y2
+    @tiles[x][y1..y2].each { |tile| tile.blocked = false }
   end
 
   def blocked?(x, y)
--- dungeon/map_objects/rectangle.rb
+++ dungeon/map_objects/rectangle.rb
@@ -7,4 +7,14 @@ class Rect
     @x2 = x + w
     @y2 = y + h
   end
+
+  def center
+    center_x = (@x1 + @x2) / 2
+    center_y = (@y1 + @y2) / 2
+    [center_x, center_y]
+  end
+
+  def intersect?(other)
+    @x1 <= other.x2 && @x2 >= other.x1 && @y1 <= other.y2 && @y2 >= other.y1
+  end
 end
