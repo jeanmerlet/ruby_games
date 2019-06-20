b8092e63ea8fec908a6ab75cd4fc4dd88cc9fbc6
Jean Merlet <jean.joseph.merlet@gmail.com>
2019-06-20 12:14:08 -0400

    update some map code

--- dungeon/map_objects/game_map.rb
+++ dungeon/map_objects/game_map.rb
@@ -1,20 +1,15 @@
 class GameMap
-  attr_accessor :tiles
+  attr_reader :tiles
 
   def initialize(width, height)
     @width = width
     @height = height
-    @tiles = initialize_tiles
-  end
-
-  def initialize_tiles
-    Array.new(@width) { Array.new(@height) { Tile.new(true) } }
+    @tiles = Array.new(@width) { Array.new(@height) { Tile.new(true) } }
   end
 
   def generate_level(min_length, max_length, max_rooms, player)
-    num_rooms = 0
     rooms = []
-    while num_rooms < max_rooms
+    max_rooms.times do |i|
       w = min_length + rand(max_length - min_length + 1)
       h = min_length + rand(max_length - min_length + 1)
       x, y = rand(@width - w), rand(@height - h)
@@ -23,30 +18,31 @@ class GameMap
       if !any_intersection?(rooms, new_room)
         create_room(new_room) 
         new_x, new_y = *new_room.center
-        if num_rooms == 0
+        if i == 0
           player.x, player.y = new_x + 1, new_y + 1
         else
           prev_x, prev_y = *rooms.last.center
           toss = rand(1)
-          if true #toss == 0
+          if toss == 0
             create_h_tunnel(prev_x, new_x, prev_y)
             create_v_tunnel(prev_y, new_y, new_x)
           else
-            #create_v_tunnel(prev_y, new_y, prev_x)
-            #create_h_tunnel(prev_x, new_x, new_y)
+            create_v_tunnel(prev_y, new_y, prev_x)
+            create_h_tunnel(prev_x, new_x, new_y)
           end
         end
         rooms << new_room
-        num_rooms += 1
       end
     end
   end
 
   def any_intersection?(rooms, new_room)
-    rooms.each do |room|
-      return true if new_room.intersect?(room)
-    end
+    rooms.each { |room| return true if new_room.intersect?(room) }
     false
+  end
+
+  def blocked?(x, y)
+    @tiles[x][y].blocked ? true : false
   end
 
   def create_room(room)
@@ -67,10 +63,5 @@ class GameMap
   def create_v_tunnel(y1, y2, x)
     y1, y2 = y2, y1 if y1 > y2
     @tiles[x][y1..y2].each { |tile| tile.blocked = false }
-  end
-
-  def blocked?(x, y)
-    return true if @tiles[x][y].blocked
-    false
   end
 end
