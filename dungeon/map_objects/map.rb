fb335369e984fd79503f9d32ca91e11c2a0248d7
Jean Merlet <jean.joseph.merlet@gmail.com>
2019-06-24 10:06:22 -0400

    circular rooms and rect-circ intersection checks

--- dungeon/map_objects/map.rb
+++ dungeon/map_objects/map.rb
@@ -1,5 +1,6 @@
 class GameMap
   attr_reader :tiles
+  include ShapeMath
 
   def initialize(width, height)
     @width = width
@@ -10,19 +11,19 @@ class GameMap
   def generate_level(min_length, max_length, max_rooms, player)
     rooms = []
     max_rooms.times do |i|
-      w = min_length + rand(max_length - min_length + 1)
-      h = min_length + rand(max_length - min_length + 1)
-      x, y = rand(@width - w), rand(@height - h)
-      new_room = Rect.new(x, y, w, h)
-
+      new_room = generate_new_room(min_length, max_length)
       if !any_intersection?(rooms, new_room)
-        create_room(new_room) 
+        create_room(new_room)
         new_x, new_y = *new_room.center
         if i == 0
-          player.x, player.y = new_x + 1, new_y + 1
+          if new_room.is_a?(Circle)
+            player.x, player.y = new_x, new_y
+          else
+            player.x, player.y = new_x + 1, new_y + 1
+          end
         else
           prev_x, prev_y = *rooms.last.center
-          toss = rand(1)
+          toss = rand(2)
           if toss == 0
             create_h_tunnel(prev_x, new_x, prev_y)
             create_v_tunnel(prev_y, new_y, new_x)
@@ -36,9 +37,33 @@ class GameMap
     end
   end
 
+  def generate_new_room(min_length, max_length)
+    toss = rand(2)
+    if toss == 0
+      r = min_length + 1 + rand(max_length - min_length + 2)
+      x, y = r + rand(@width - 2*r), r + rand(@height - 2*r)
+      Circle.new(x, y, r)
+    else
+      w = 2*min_length + rand(2*(max_length - min_length) + 1)
+      h = 2*min_length + rand(2*(max_length - min_length) + 1)
+      x, y = rand(@width - w), rand(@height - h)
+      Rect.new(x, y, w, h)
+    end
+  end
+
   def any_intersection?(rooms, new_room)
-    rooms.each { |room| return true if new_room.intersect?(room) }
+    rooms.each { |room| return true if intersect?(new_room, room) }
     false
+  end
+
+  def intersect?(room1, room2)
+    if room1.class.name.split('::').last == room2.class.name.split('::').last
+      room1.intersect?(room2)
+    elsif room1.is_a?(Circle)
+      circ_rect_intersect?(room1, room2)
+    else
+      circ_rect_intersect?(room2, room1)
+    end
   end
 
   def blocked?(x, y)
@@ -46,11 +71,47 @@ class GameMap
   end
 
   def create_room(room)
+    if room.is_a?(Circle)
+      create_circ_room(room)
+    elsif room.is_a?(Rect)
+      create_rect_room(room)
+    end
+  end
+
+  def create_rect_room(room)
     @tiles[room.x1+1..room.x2].each do |h_tile_line|
       h_tile_line[room.y1+1..room.y2].each do |tile|
         tile.blocked = false
       end
     end
+  end
+
+  def create_circ_room(room)
+    x, r = 0, room.r - 0.7
+    quadrant = []
+    room.r.times do |i|
+      x += 1
+      y = 0
+      room.r.times do
+        y += 1
+        quadrant << [x + room.x, y + room.y] if x*x + y*y <= r*r
+      end
+    end
+
+    half = quadrant + quadrant.map { |coords| [2*room.x - coords[0], coords[1]] }
+    room.r.times do |i|
+      half << [room.x, room.y + i]
+    end
+    whole = half + half.map { |coords| [coords[0], 2*room.y - coords[1]] }
+    (room.r*2 - 1).times do |i|
+      whole << [room.x - room.r + i + 1, room.y]
+    end
+
+    whole.each { |xy| @tiles[xy[0]][xy[1]].blocked = false }
+  end
+
+  def connect_rooms(room1, room2)
+    
   end
 
   def create_h_tunnel(x1, x2, y)
--- dungeon/map_objects/shapes.rb
+++ dungeon/map_objects/shapes.rb
@@ -1,11 +1,34 @@
+module ShapeMath
+  def circ_rect_intersect?(circ, rect)
+    if rect.x1 <= circ.x && circ.x <= rect.x2 && rect.y1 <= circ.y && circ.y <= rect.y2
+      (circ.y - rect.y1).abs <= circ.r ||
+      (circ.y - rect.y2).abs <= circ.r ||
+      (circ.x - rect.x1).abs <= circ.r ||
+      (circ.x - rect.x2).abs <= circ.r
+    elsif rect.x1 <= circ.x && circ.x <= rect.x2
+      (circ.y - rect.y1).abs <= circ.r ||
+      (circ.y - rect.y2).abs <= circ.r
+    elsif rect.y1 <= circ.y && circ.y <= rect.y2
+      (circ.x - rect.x1).abs <= circ.r ||
+      (circ.x - rect.x2).abs <= circ.r
+    else
+      (circ.x-rect.x1)**2 + (circ.y-rect.y1)**2 < circ.r**2 ||
+      (circ.x-rect.x1)**2 + (circ.y-rect.y2)**2 < circ.r**2 ||
+      (circ.x-rect.x2)**2 + (circ.y-rect.y1)**2 < circ.r**2 ||
+      (circ.x-rect.x2)**2 + (circ.y-rect.y2)**2 < circ.r**2
+    end
+  end
+end
+
 class Rect
-  attr_reader :x1, :y1, :x2, :y2
+  attr_reader :x1, :y1, :x2, :y2, :center
 
   def initialize(x, y, w, h)
     @x1 = x
     @y1 = y
     @x2 = x + w
     @y2 = y + h
+    @center = center
   end
 
   def center
@@ -14,39 +37,25 @@ class Rect
     [center_x, center_y]
   end
 
-  def intersect?(other)
-    @x1 <= other.x2 && @x2 >= other.x1 && @y1 <= other.y2 && @y2 >= other.y1
+  def intersect?(rect)
+    @x1 <= rect.x2 && @x2 >= rect.x1 && @y1 <= rect.y2 && @y2 >= rect.y1
   end
 end
 
 class Circle
-  attr_reader :x, :y, :r, :midpoint
+  attr_reader :x, :y, :r, :center
 
   def initialize(x, y, r)
     @x = x
     @y = y
     @r = r
-    @midpoint = ((r-0.5) / Math.sqrt(2)).round
-    print "r: #{@r} m: #{@midpoint}\n"
-    @border = initialize_border
+    @center = [@x, @y]
   end
 
-  def initialize_border
-    border = [[@r, 0], [@x/2, @y/2]]
-    x, y = @x + @r, @y
-    while x > (@x/2) && y < (@y/2)
-      y -= 1
-      if true
-      end
-    end
-  end
-
-  def center
-    [x, y]
-  end
-
-  def contains?(x, y)
+  def intersect?(circle)
+    x = @x - circle.x
+    y = @y - circle.y
+    r = @r + circle.r
+    x*x + y*y < r*r
   end
 end
-
-10.times { |i| Circle.new(0, 0, i+1) }
